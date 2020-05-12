3de78ff724d324a84bcc6d0f207ef8a2758ee1ad
Leo LECLERC <leo.leclerc@imt-atlantique.net>
2020-05-12 14:39:28 +0200

    Initial commit

---
+++ src/main/java/outils/App.java
@@ -0,0 +1,17 @@
+package outils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        print();
+    }
+
+    private static void print() {
+        System.out.println( "Hello World!" );
+    }
+}
